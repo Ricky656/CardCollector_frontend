cb0e5bf52320b9003d11cca051212a35e24fd779
Alaric Sheer Hardwick <85938018+Alaric656@users.noreply.github.com>
2025-07-13 18:20:12 +0100

    Fixed z-index when dragging an item

--- card-collector-frontend/src/Components/Util/Draggable.jsx
+++ card-collector-frontend/src/Components/Util/Draggable.jsx
@@ -5,7 +5,7 @@ export function Draggable(props){
     const {attributes, listeners, setNodeRef, transform} = useDraggable({id: props.id});
 
     const style = transform ? 
-        {transform: `translate(${transform.x}px, ${transform.y}px)`} : undefined;
+        {transform: `translate(${transform.x}px, ${transform.y}px)`, zIndex: 12} : undefined;
         
     return(
         <div ref={setNodeRef} style={style} {...listeners} {...attributes}>
